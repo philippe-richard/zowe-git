8ffb22477b0582386a28d8e99e71f177e7a36135
philippeHelloWorld.class-richard <philippe_richard@fr.ibm.com>
2018-11-19 17:13:55 +0100

    chg credentials

--- zowe_submit_cred.sh
+++ zowe_submit_cred.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set +x
 echo 'jenkins'
-zowe profiles create zosmf-profile tx9 --host tx9 --port 9143 --$ZOWE_OPT_USER --pass $ZOWE_OPT_USER_PWD --reject-unauthorized false --overwrite
+zowe profiles create zosmf-profile tx9 --host tx9 --port 9143 -u $ZOWE_OPT_USER --pass $ZOWE_OPT_USER_PWD --reject-unauthorized false --overwrite
 #bright profiles create zosmf tso1 --host tso1 --port 443 -u $CREDENTIALS_USR -p $CREDENTIALS_PSW --no-ru
