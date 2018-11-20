90525f1fd4ea2c3fcfabcbb2352bdc9ec5d0ebb5
philippeHelloWorld.class-richard <philippe_richard@fr.ibm.com>
2018-11-20 16:40:19 +0100

    chg credentials

--- zowe_submit_job.sh
+++ zowe_submit_job.sh
@@ -52,5 +52,5 @@ submitJCL "PRICHAR.X9.JCL(IEFBR14)"
 	zowe zos-jobs view sfbi $jobid 4 -u prichar --pw s99arlat --ru false
 echo 'job id is :' $jobid
 echo 'WTO ' $jobid 'has executed'
-zowe console issue cmd '''SE ''$jobid'' was submitted and completed by ZOWE''' -u prichar --pw s99arlat --ru false
+zowe console issue cmd 'SE '''$jobid''' was submitted and completed by ZOWE' -u prichar --pw s99arlat --ru false
  
