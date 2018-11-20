ffd35ac751adc0fcb32a1c4e6e1300bb7b2a4ef3
philippeHelloWorld.class-richard <philippe_richard@fr.ibm.com>
2018-11-20 16:31:46 +0100

    chg credentials

--- zowe_submit_job.sh
+++ zowe_submit_job.sh
@@ -51,5 +51,6 @@ submitJCL "PRICHAR.X9.JCL(IEFBR14)"
 	zowe zos-jobs view sfbi $jobid 3 -u prichar --pw s99arlat --ru false
 	zowe zos-jobs view sfbi $jobid 4 -u prichar --pw s99arlat --ru false
 echo 'job id is :' $jobid
-echo 'export jobid='$jobid '> myenv'
+echo 'WTO ' $jobid 'has executed'
+zowe console issue cmd "SE '$jobid' was submitted and completed by ZOWE" -u prichar --pw s99arlat --ru false
  
--- zowe_submit_output.sh
+++ zowe_submit_output.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 set +x
 
- // unstash the temp file and apply it
-                 source ./myenv
+ 
+ #                source ./myenv
 
-                // now continue on with variables set 
-                echo jobid is :  $jobid
+                
+ #               echo jobid is :  $jobid
 	
 	echo 'job completed in OUTPUT status. Final result of job:'
-	zowe zos-jobs list spool-files-by-jobid $jobid -u prichar --pw s99arlat --ru false
-	zowe zos-jobs view sfbi $jobid 2 -u prichar --pw s99arlat --ru false
-	zowe zos-jobs view sfbi $jobid 3 -u prichar --pw s99arlat --ru false
-	zowe zos-jobs view sfbi $jobid 4 -u prichar --pw s99arlat --ru false
+#	zowe zos-jobs list spool-files-by-jobid $jobid -u prichar --pw s99arlat --ru false
+#	zowe zos-jobs view sfbi $jobid 2 -u prichar --pw s99arlat --ru false
+#	zowe zos-jobs view sfbi $jobid 3 -u prichar --pw s99arlat --ru false
+#	zowe zos-jobs view sfbi $jobid 4 -u prichar --pw s99arlat --ru false
 	
 	echo ''
-echo 'WTO $jobid has executed'
-zowe console issue cmd "SE '$jobid' was submitted and completed by ZOWE" -u prichar --pw s99arlat --ru false
+
