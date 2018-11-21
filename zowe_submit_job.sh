5410465a8010e84de78f84cb64f47084cffba6b8
philippeHelloWorld.class-richard <philippe_richard@fr.ibm.com>
2018-11-21 09:26:28 +0100

    chg credentials

--- zowe_submit_job.sh
+++ zowe_submit_job.sh
@@ -53,5 +53,7 @@ echo 'job completed in OUTPUT status. Final result of job:'
 echo 'job id is :' $jobid
 echo 'WTO ' $jobid 'has executed'
 
-zowe console issue cmd "SE ''''$jobid'' was submitted and completed by ZOWE...'" -u $userid --pw $password --ru false
+zowe console issue cmd "SE '''$jobid' was submitted and completed by ZOWE...'" -u $userid --pw $password --ru false
+echo $RESULT
+if [ $RESULT -eq 1641 ]; then exit 0; fi
  
