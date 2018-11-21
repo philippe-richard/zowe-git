863ee63470a187ef4bec8664e31a40affc41089e
philippeHelloWorld.class-richard <philippe_richard@fr.ibm.com>
2018-11-21 08:52:28 +0100

    chg credentials

--- zowe_submit_job.sh
+++ zowe_submit_job.sh
@@ -5,12 +5,12 @@ set +x
 #zowe zosmf check status --zosmf-profile tx9 -H 9.212.128.238 -P 9143 -u prichar --pw s99arlat --ru false
 #zowe zos-jobs submit data-set "prichar.x9.jcl(iefbr14)" -u prichar --pw s99arlat --ru false
 echo "Downloading the IEFBR14 program"
-#zowe files dl ds "prichar.x9.jcl(iefbr14)" -f iefbr14.jcl -u prichar --pw s99arlat --ru false
+zowe files dl ds "prichar.x9.jcl(iefbr14)" -f iefbr14.jcl -u $userid --pw $password --ru false 
 echo "Editing iefbr14 locally to update it"
 echo ""
 #notepad iefbr14.jcl 
 echo "Uploading the IEFBR14 program"
-#zowe files ul ftds iefbr14.jcl "prichar.x9.jcl(iefbr14)" -u prichar --pw s99arlat --ru false
+zowe files ul ftds iefbr14.jcl "prichar.x9.jcl(iefbr14)" -u $userid --pw $password --ru false 
 
 echo ""
 echo "Submitting the iefbr14 program"
@@ -21,12 +21,12 @@ function submitJCL () {
     ds=$1
 
     echo 'zowe zos-jobs submit data-set "'$ds'" --rff jobid --rft string'
-    jobid=`zowe zos-jobs submit data-set $ds --rff jobid --rft string -u prichar --pw s99arlat --ru false `
+    jobid=`zowe zos-jobs submit data-set $ds --rff jobid --rft string -u $userid --pw $password --ru false`
     echo $jobid
     echo ''
 
     echo 'zowe zos-jobs view job-status-by-jobid' $jobid '--rff retcode --rft string'
-    retcode=`zowe zos-jobs view job-status-by-jobid $jobid --rff retcode --rft string -u prichar --pw s99arlat --ru false`
+    retcode=`zowe zos-jobs view job-status-by-jobid $jobid --rff retcode --rft string -u $userid --pw $password --ru false`
     echo $retcode
     echo ''
     
@@ -46,12 +46,12 @@ function submitJCL () {
 
 submitJCL "PRICHAR.X9.JCL(IEFBR14)"
 echo 'job completed in OUTPUT status. Final result of job:'
-#	zowe zos-jobs list spool-files-by-jobid $jobid -u prichar --pw s99arlat --ru false
-#	zowe zos-jobs view sfbi $jobid 2 -u prichar --pw s99arlat --ru false
-#	zowe zos-jobs view sfbi $jobid 3 -u prichar --pw s99arlat --ru false
-#	zowe zos-jobs view sfbi $jobid 4 -u prichar --pw s99arlat --ru false
+	zowe zos-jobs list spool-files-by-jobid $jobid -u $userid --pw $password --ru false
+	zowe zos-jobs view sfbi $jobid 2 -u $userid --pw $password --ru false
+	zowe zos-jobs view sfbi $jobid 3 -u $userid --pw $password --ru false
+	zowe zos-jobs view sfbi $jobid 4 -u $userid --pw $password --ru false
 echo 'job id is :' $jobid
 echo 'WTO ' $jobid 'has executed'
 
-zowe console issue cmd "SE '''$jobid'' was submitted and completed by ZOWE...'" -u prichar --pw s99arlat --ru false
+zowe console issue cmd "SE '''$jobid'' was submitted and completed by ZOWE...'" -u $userid --pw $password --ru false
  
