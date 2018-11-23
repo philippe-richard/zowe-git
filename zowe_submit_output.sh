660b9acf500f576a773d912700c24ab2dfdf5d2b
philippeHelloWorld.class-richard <philippe_richard@fr.ibm.com>
2018-11-23 08:43:34 +0100

    chg credentials

--- zowe_submit_output.sh
+++ zowe_submit_output.sh
@@ -3,12 +3,17 @@ export jobid=$@
 echo jobid is: $jobid 
 set +x
 
+
+zowe console issue cmd "SE '$jobid was submitted and completed by ZOWE...'" -u $userid --pw $password --ru false
+RESULT=$?
+echo WTO rc is :  $RESULT
+if [ $RESULT -eq 1641 ]; then exit 0; fi
  
+ echo 'WTO ' $jobid 'has executed'
  #                source ./myenv
 
                 
- #               echo jobid is :  $jobid
-	
+ 	
 	echo 'job completed in OUTPUT status. Final result of job:'
 #	zowe zos-jobs list spool-files-by-jobid $jobid -u prichar --pw s99arlat --ru false
 #	zowe zos-jobs view sfbi $jobid 2 -u prichar --pw s99arlat --ru false
